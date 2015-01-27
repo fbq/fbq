74a01c25a242beee90d62accd2040d5a995dcf8a
Boqun Feng <boqun.feng@gmail.com>
2015-01-27 12:01:02 +0800

    Initial commit

---
+++ about.markdown
@@ -0,0 +1,22 @@
+---
+title: About
+---
+My name is 冯博群(Boqun Feng).
+
+I was born at the Northeast of China, got my BS and MS degrees at SJTU in Shanghai.
+
+Now I'm working at IBM LTC(Linux Technology Center) now. I'm still a Linux kernel newbie,
+my kernel patches can be found [here](https://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/log/?qt=author&q=Boqun).
+
+I like working on open source projects, and the projects I also got involved are
+[beignet](http://cgit.freedesktop.org/beignet/) and
+[OSv](https://github.com/cloudius-systems/osv).
+One thing I feel great about open source projects is that I'm free to make any improvement
+to the software and my work can be reviewed and improved by people from all over the world,
+and thhat's really exciting.
+
+I'm interested in system programming(OS, virtualization, etc.),
+programming languages(Theories and Implementations, i.e., compilers or interpreters)
+and mathematics. I think everyone has his or her rights to use the life, and I want
+to use it as a journey for truth. So the title of the blog, 朝闻道, is a good discription
+for my attitude towards life.
---
+++ site.hs
@@ -0,0 +1,128 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend)
+import           Hakyll
+
+--------------------------------------------------------------------------------
+-- Site Configuration
+
+title :: String
+title = "朝闻道"
+
+description :: String
+description = "For the Eureka!"
+
+host :: String
+host = "blog.fixme.name"
+
+authorName :: String
+authorName = "Boqun Feng"
+
+authorEmail :: String
+authorEmail = "boqun.feng@gmail.com"
+
+myFeedConfiguration :: FeedConfiguration
+myFeedConfiguration = FeedConfiguration
+    { feedTitle       = title
+    , feedDescription = description
+    , feedAuthorName  = authorName
+    , feedAuthorEmail = authorEmail
+    , feedRoot        = "http://" ++ host
+    }
+
+-- add "date" to post context
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+    -- basic static files for blog
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    -- about page
+    match "about.markdown" $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
+    -- all posts
+    match "posts/*" $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+            -- save a snaphot for feed
+            >>= saveSnapshot "content"
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= relativizeUrls
+
+    -- archive of all posts
+    create ["archive.html"] $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            makeListItem posts postCtx "Archive" "templates/archive.html"
+
+    -- all notes
+    match "notes/**" $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/note.html"    postCtx
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= relativizeUrls
+
+    -- index of all notes
+    create ["notes.html"] $ do
+        route idRoute
+        compile $ do
+            notes <- loadAll "notes/**"
+            makeListItem notes defaultContext "Notes" "templates/notes.html"
+
+    -- index page
+    match "index.html" $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let indexCtx =
+                    listField "items" postCtx (return posts) `mappend`
+                    defaultContext
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+    -- templates
+    match "templates/*" $ compile templateCompiler
+
+    -- feed
+    create ["atom.xml"] $ do
+        route idRoute
+        compile $ do
+            let feedCtx = postCtx `mappend` bodyField "description"
+            posts <- recentFirst =<<
+                loadAllSnapshots "posts/*" "content"
+            renderAtom myFeedConfiguration feedCtx posts
+
+
+--------------------------------------------------------------------------------
+makeListItem :: [Item a]-> Context a -> String -> Identifier -> Compiler (Item String)
+makeListItem items itemCtx title template = do
+    let archiveCtx =
+            listField "items" itemCtx (return items) `mappend`
+            constField "title" title                 `mappend`
+            defaultContext
+
+    makeItem ""
+        >>= loadAndApplyTemplate template archiveCtx
+        >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+        >>= relativizeUrls
